c0ad5be3258fa5e5717e56a145f975a0eb6713c2
Ryo <gryo.1440@gmail.com>
2022-06-21 12:06:50 +0900

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,94 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+
+import Board from './components/Board'
+
+import calculateWinner from './methods/calculateWinner'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Game />
   );
+}
+
+class Game extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      history: [{
+        squares: Array(9).fill(null),
+      }],
+      xIsNext: true,
+      stepNumber: 0,
+    };
+  }
+
+  handleClick(i) {
+    // Don't mutate the data by directly changing the data’s values, because of immutaiblity
+    const history = this.state.history.slice(0, this.state.stepNumber + 1);
+    const current = history[history.length - 1];
+    const squares = current.squares.slice();
+
+    if (calculateWinner(squares) || squares[i]) {
+      // 以下のreturnにより、この条件下ではこれより後の処理がスキップ
+      return;
+    }
+
+    squares[i] = this.state.xIsNext ? 'X' : 'O';
+    this.setState({
+      history: history.concat([{
+        squares: squares,
+      }]),
+      xIsNext: !this.state.xIsNext,
+      stepNumber: history.length,
+    });
+  }
+
+  jumpTo(step) {
+    this.setState({
+      xIsNext: (step % 2) === 0,
+      stepNumber: step,
+    });
+  }
+
+  render() {
+    const history = this.state.history;
+    const current = history[this.state.stepNumber];
+    const winner = calculateWinner(current.squares);
+    let status = '';
+    if (winner) {
+      status = 'Winner: ' + winner;
+    } else {
+      status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O');
+    }
+
+    const moves = history.map((board, index) => {
+      // indexが配列historyでのindexで、boardがそのindexの値
+      const desc = index ?
+        'Go to move #' + index :
+        'Go to game start';
+      return (
+        <li key={index}>
+          <button onClick={() => this.jumpTo(index)}>{desc}</button>
+        </li>
+      );
+    });
+
+    return (
+      <div className="game">
+        <div className="game-board">
+          <Board
+            squares={current.squares}
+            onClick={(i) => this.handleClick(i)}
+          />
+        </div>
+        <div className="game-info">
+          <div>{status}</div>
+          <ol>{moves}</ol>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ src/components/Board.js
@@ -0,0 +1,39 @@
+import React from 'react';
+
+import Square from './Square'
+
+class Board extends React.Component {
+    renderBoardRow(i) {
+        const squareNumArray = [i * 3, i * 3 + 1, i * 3 + 2];
+        const boardRow = squareNumArray.map((num) =>
+        <Square
+            key={num.toString()}
+            value={this.props.squares[num]}
+            onClick={() => this.props.onClick(num)}
+        />
+        );
+
+        return boardRow;
+        // 以下の書き方はエラー
+        // return ({boardRow});
+    }
+
+    render() {
+        const rowArray = [0, 1, 2];
+        const boardRows = rowArray.map((row) => {
+        return (
+            <div key={row.toString()} className='board-row'>
+            {this.renderBoardRow(row)}
+            </div>
+        );
+        });
+
+        return (
+        <div>
+            {boardRows}
+        </div>
+        );
+    }
+}
+
+export default Board;
